691dd28136462683d999eeb17c76e90d7bb3ffd8
DmitMark <dmmarkushevski@gmail.com>
2023-05-03 16:56:17 +0300

    new file HW(18.8.19) add

---
+++ Задание 18.8.19 (HW-03).py
@@ -0,0 +1,15 @@
+num = int(input('Введите количество билетов '))
+item = 1
+price = 0
+while item <= num:
+    year = int(input('Введите возраст посетителя '))
+    if year >= 18:
+        if year < 25:
+            price += 990
+        elif year >= 25:
+            price += 1390
+    item += 1
+if num > 3:
+    print('Сумма к оплате, с учетом 10% скидки ', price * 0.9, 'руб')
+else:
+    print('Сумма к оплате', price, 'руб')
